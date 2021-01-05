e40367b9a7b4a8736b34c5c89d8a44d49ffde74a
pinglamb <pinglambs@gmail.com>
2021-01-05 19:25:35 +0800

    ✨ Implemented no votes fork choice rules

--- lib/eth2-fork-choice.rb
+++ lib/eth2-fork-choice.rb
@@ -2,4 +2,254 @@
 
 require_relative 'eth2-fork-choice/version'
 
-module Eth2ForkChoice; end
+module Eth2ForkChoice
+  ZERO_HASH = '0000000000000000000000000000000000000000000000000000000000000000'
+
+  class UnknownJustifiedRoot < StandardError; end
+
+  class Magic
+    attr_reader :store
+
+    def head(justified_epoch, justified_root, finalized_epoch)
+      delta = [0] * @store.nodes.size
+      @store.apply_weight_changes(justified_epoch, finalized_epoch, delta)
+      @store.head(justified_root)
+    end
+
+    def process_block(slot, block_root, parent_root, justified_epoch, finalized_epoch)
+      @store.insert(slot, block_root, parent_root, justified_epoch, finalized_epoch)
+    end
+
+    private
+
+    def initialize(justified_epoch, finalized_epoch, finalized_root)
+      @store =
+        Store.new(justified_epoch: justified_epoch, finalized_epoch: finalized_epoch, finalized_root: finalized_root)
+    end
+  end
+
+  class Store
+    attr_reader :nodes_indices, :nodes
+    attr_accessor :justified_epoch, :finalized_epoch, :finalized_root
+
+    def head(justified_root)
+      justified_index = @nodes_indices[justified_root]
+      raise UnknownJustifiedRoot if justified_index.nil?
+
+      justified_node = @nodes[justified_index]
+      best_descendent_index = justified_node.best_descendant
+      best_descendent_index = justified_index if best_descendent_index.nil?
+      best_node = @nodes[best_descendent_index]
+      unless viable_for_head?(best_node)
+        raise StandardError.new(
+                "head at slot #{best_node.slot} with weight #{
+                  (best_node.weight / 10e9).floor
+                } is not eligible, finalized_epoch #{best_node.finalized_epoch} != #{
+                  @finalized_epoch
+                }, justified_epoch #{best_node.justified_epoch} != #{@justified_epoch}"
+              )
+      end
+
+      best_node.root
+    end
+
+    def insert(slot, root, parent, justified_epoch, finalized_epoch)
+      # Return if the block has been inserted into Store before.
+      return unless @nodes_indices[root].nil?
+
+      index = @nodes.size
+      parent_index = @nodes_indices[parent]
+
+      node =
+        Node.new(
+          slot: slot,
+          root: root,
+          parent: parent_index,
+          justified_epoch: justified_epoch,
+          finalized_epoch: finalized_epoch,
+          best_child: nil,
+          best_descendant: nil,
+          weight: 0
+        )
+
+      @nodes_indices[root] = index
+      @nodes << node
+
+      update_best_child_and_descendant(parent_index, index) unless node.parent.nil?
+
+      node
+    end
+
+    # applyWeightChanges iterates backwards through the nodes in store. It checks all nodes parent
+    # and its best child. For each node, it updates the weight with input delta and
+    # back propagate the nodes delta to its parents delta. After scoring changes,
+    # the best child is then updated along with best descendant.
+    def apply_weight_changes(justified_epoch, finalized_epoch, delta)
+      # Update the justified / finalized epochs in store if necessary.
+      if @justified_epoch != justified_epoch || @finalized_epoch != finalized_epoch
+        @justified_epoch = justified_epoch
+        @finalized_epoch = finalized_epoch
+      end
+
+      last_index = @nodes.size - 1
+
+      # Iterate backwards through all index to node in store.
+      last_index.downto(0) do |i|
+        node = @nodes[i]
+
+        # There is no need to adjust the balances or manage parent of the zero hash, it
+        # is an alias to the genesis block.
+        next if node == ZERO_HASH
+
+        node_delta = delta[i]
+        if node_delta < 0
+          # A node's weight can not be negative but the delta can be negative.
+          if node.weight + node_delta < 0
+            node.weight = 0
+          else
+            # Subtract node's weight.
+            node.weight -= node_delta.abs
+          end
+        else
+          node.weight += node_delta
+        end
+
+        delta[node.parent] += node_delta unless node.parent.nil?
+      end
+
+      last_index.downto(0) do |i|
+        node = @nodes[i]
+        update_best_child_and_descendant(node.parent, i) unless node.parent.nil?
+      end
+    end
+
+    private
+
+    def initialize(justified_epoch:, finalized_epoch:, finalized_root:)
+      @justified_epoch = justified_epoch
+      @finalized_epoch = finalized_epoch
+      @finalized_root = finalized_root
+      @nodes_indices = {}
+      @nodes = []
+    end
+
+    # updateBestChildAndDescendant updates parent node's best child and descendent.
+    # It looks at input parent node and input child node and potentially modifies parent's best
+    # child and best descendent indices.
+    # There are four outcomes:
+    # 1.)  The child is already the best child but it's now invalid due to a FFG change and should be removed.
+    # 2.)  The child is already the best child and the parent is updated with the new best descendant.
+    # 3.)  The child is not the best child but becomes the best child.
+    # 4.)  The child is not the best child and does not become best child.
+    def update_best_child_and_descendant(parent_index, child_index)
+      parent = @nodes[parent_index]
+      child = @nodes[child_index]
+
+      child_leads_to_viable_head = leads_to_viable_head?(child)
+
+      # Define 3 variables for the 3 outcomes mentioned above. This is to
+      # set `parent.bestChild` and `parent.bestDescendant` to. These
+      # aliases are to assist readability.
+      change_to_none = [nil, nil]
+      change_to_child = [child_index, child.best_descendant || child_index]
+      no_change = [parent.best_child, parent.best_descendant]
+
+      result = nil
+      if parent.best_child.nil?
+        if child_leads_to_viable_head
+          # If parent doesn't have a best child and the child is viable.
+          result = change_to_child
+        else
+          # If parent doesn't have a best child and the child is not viable.
+          result = no_change
+        end
+      else
+        if parent.best_child == child_index && !child_leads_to_viable_head
+          # If the child is already the best child of the parent but it's not viable for head,
+          # we should remove it. (Outcome 1)
+          result = change_to_none
+        elsif parent.best_child == child_index
+          # If the child is already the best child of the parent, set it again to ensure best
+          # descendent of the parent is updated. (Outcome 2)
+          result = change_to_child
+        else
+          best_child = @nodes[parent.best_child]
+
+          # Is current parent's best child viable to be head? Based on justification and finalization rules.
+          best_child_leads_to_viable_head = leads_to_viable_head?(best_child)
+
+          if child_leads_to_viable_head && !best_child_leads_to_viable_head
+            # The child leads to a viable head, but the current parent's best child doesnt.
+            result = change_to_child
+          elsif !child_leads_to_viable_head && best_child_leads_to_viable_head
+            # The child doesn't lead to a viable head, the current parent's best child does.
+            result = no_change
+          elsif child.weight == best_child.weight
+            # If both are viable, compare their weights.
+            # Tie-breaker of equal weights by root.
+            # FIXME Simplified comparison, just first 2 bytes
+            child_root_bytes = [child.root].pack('H*').bytes
+            best_child_root_bytes = [best_child.root].pack('H*').bytes
+            child_root_gt_best_child_root =
+              if child_root_bytes[0] == best_child_root_bytes[0]
+                child_root_bytes[1] >= best_child_root_bytes[1]
+              else
+                child_root_bytes[0] > best_child_root_bytes[0]
+              end
+            if child_root_gt_best_child_root
+              result = change_to_child
+            else
+              result = no_change
+            end
+          else
+            if child.weight > best_child.weight
+              result = change_to_child
+            else
+              result = no_change
+            end
+          end
+        end
+      end
+
+      parent.best_child, parent.best_descendant = *result
+
+      parent
+    end
+
+    # leadsToViableHead returns true if the node or the best descendent of the node is viable for head.
+    # Any node with diff finalized or justified epoch than the ones in fork choice store
+    # should not be viable to head.
+    def leads_to_viable_head?(node)
+      node.best_descendant.nil? ? viable_for_head?(node) : viable_for_head?(@nodes[node.best_descendant])
+    end
+
+    # viableForHead returns true if the node is viable to head.
+    # Any node with diff finalized or justified epoch than the ones in fork choice store
+    # should not be viable to head.
+    def viable_for_head?(node)
+      # `node` is viable if its justified epoch and finalized epoch are the same as the one in `Store`.
+      # It's also viable if we are in genesis epoch.
+      justified = @justified_epoch == node.justified_epoch || @justified_epoch == 0
+      finalized = @finalized_epoch == node.finalized_epoch || @finalized_epoch == 0
+
+      justified && finalized
+    end
+  end
+
+  class Node
+    attr_accessor :slot, :root, :parent, :justified_epoch, :finalized_epoch, :best_child, :best_descendant, :weight
+
+    private
+
+    def initialize(slot:, root:, parent:, justified_epoch:, finalized_epoch:, best_child:, best_descendant:, weight:)
+      @slot = slot
+      @root = root
+      @parent = parent
+      @justified_epoch = justified_epoch
+      @finalized_epoch = finalized_epoch
+      @best_child = best_child
+      @best_descendant = best_descendant
+      @weight = weight
+    end
+  end
+end
--- spec/eth2_fork_choice_spec.rb
+++ spec/eth2_fork_choice_spec.rb
@@ -1,4 +1,99 @@
 # frozen_string_literal: true
 
+require 'digest'
+
+ZERO_HASH = Eth2ForkChoice::ZERO_HASH
+
+def index_to_hash(i)
+  Digest::SHA256.hexdigest([i].pack('Q<'))
+end
+
 RSpec.describe Eth2ForkChoice do
+  context 'No Vote' do
+    it 'can find head' do
+      f = Eth2ForkChoice::Magic.new(0, 0, ZERO_HASH)
+      f.process_block(0, ZERO_HASH, nil, 1, 1)
+
+      # The head should always start at the finalized block.
+      r = f.head(1, ZERO_HASH, 1)
+      expect(r).to eq(ZERO_HASH)
+
+      # Insert block 2 into the tree and verify head is at 2:
+      #         0
+      #        /
+      #       2 <- head
+      f.process_block(0, index_to_hash(2), ZERO_HASH, 1, 1)
+      r = f.head(1, ZERO_HASH, 1)
+      expect(r).to eq(index_to_hash(2))
+
+      # Insert block 1 into the tree and verify head is still at 2:
+      #            0
+      #           / \
+      #  head -> 2  1
+      f.process_block(0, index_to_hash(1), ZERO_HASH, 1, 1)
+      r = f.head(1, ZERO_HASH, 1)
+      expect(r).to eq(index_to_hash(2))
+
+      # Insert block 3 into the tree and verify head is still at 2:
+      #            0
+      #           / \
+      #  head -> 2  1
+      #             |
+      #             3
+      f.process_block(0, index_to_hash(3), index_to_hash(1), 1, 1)
+      r = f.head(1, ZERO_HASH, 1)
+      expect(r).to eq(index_to_hash(2))
+
+      # Insert block 4 into the tree and verify head is at 4:
+      #            0
+      #           / \
+      #          2  1
+      #          |  |
+      #  head -> 4  3
+      f.process_block(0, index_to_hash(4), index_to_hash(2), 1, 1)
+      r = f.head(1, ZERO_HASH, 1)
+      expect(r).to eq(index_to_hash(4))
+
+      # Insert block 5 with justified epoch of 2, verify head is still at 4.
+      #            0
+      #           / \
+      #          2  1
+      #          |  |
+      #  head -> 4  3
+      #          |
+      #          5 <- justified epoch = 2
+      f.process_block(0, index_to_hash(5), index_to_hash(4), 2, 1)
+      r = f.head(1, ZERO_HASH, 1)
+      expect(r).to eq(index_to_hash(4))
+
+      expect { f.head(1, index_to_hash(5), 1) }.to raise_error { |e|
+        e.message == 'head at slot 0 with weight 0 is not eligible, finalized_epoch 1 != 1, justified_epoch 2 != 1'
+      }
+
+      # Set the justified epoch to 2 and start block to 5 to verify head is 5.
+      #            0
+      #           / \
+      #          2  1
+      #          |  |
+      #          4  3
+      #          |
+      #          5 <- head
+      r = f.head(2, index_to_hash(5), 1)
+      expect(r).to eq(index_to_hash(5))
+
+      # Insert block 6 with justified epoch of 2, verify head is at 6.
+      #            0
+      #           / \
+      #          2  1
+      #          |  |
+      #          4  3
+      #          |
+      #          5
+      #          |
+      #          6 <- head
+      f.process_block(0, index_to_hash(6), index_to_hash(5), 2, 1)
+      r = f.head(2, index_to_hash(5), 1)
+      expect(r).to eq(index_to_hash(6))
+    end
+  end
 end
